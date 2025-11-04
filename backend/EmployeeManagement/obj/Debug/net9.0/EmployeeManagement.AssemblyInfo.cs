277788d5315684837a6ff203f5aeac86fac7c935
FeifanWang1029 <exwang1029@outlook.com>
2025-11-05 00:02:33 +1100

    fix docker

--- backend/EmployeeManagement/obj/Debug/net9.0/EmployeeManagement.AssemblyInfo.cs
+++ backend/EmployeeManagement/obj/Debug/net9.0/EmployeeManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8f2a1ecdcb486b3ec2f04a67f346669f9939f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd3be1d08d7d1816b5b7f4d7d422608041a9b0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
